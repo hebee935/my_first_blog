d30e117469a185631e4011b9b7b03867d2e4e712
Eunbee Hong <erica990305@e-mirim.hs.kr>
2016-06-06 00:41:34 +0900

    edit views.py

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ from .forms import PostForm
 
 # Create your views here.
         
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+    
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
@@ -13,8 +17,4 @@ def post_list(request):
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
-        
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
         
