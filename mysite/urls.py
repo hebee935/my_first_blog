dd6eb338366778d1370779a25eb48cde707e2a29
Eunbee Hong <erica990305@e-mirim.hs.kr>
2016-06-06 01:28:33 +0900

    edit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+include django.contrib.auth.views
 
-urlpatterns = [
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'',include('blog.urls')),
-]
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls')),
+)
